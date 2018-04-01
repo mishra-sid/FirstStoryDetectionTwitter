a2590894932a2a212b607cad61064c753c8a4f93
sid <es15btech11018@iith.ac.in>
2018-04-01 14:23:48 +0530

    Add optimized TFIDF, Hash function helper

--- hash_helper.py
+++ hash_helper.py
@@ -2,41 +2,40 @@ from fraction import gcd
 from random import randint
 from constants import *
 
-class hash_helper:
-    def get_sieve(N):
-        prime = [1] * (N + 1)
-        prime[0] = 0
-        prime[1] = 0
-        for i in range(N + 1):
-            if prime[i]:
-                for j in range(i * 2, N + 1, i):
-                    prime[j] = 0
-        primes = []
-        for i in range(N + 1):
-            if prime[i]:
-                primes.append(i)
+def get_sieve(N):
+    prime = [1] * (N + 1)
+    prime[0] = 0
+    prime[1] = 0
+    for i in range(N + 1):
+        if prime[i]:
+            for j in range(i * 2, N + 1, i):
+                prime[j] = 0
+    primes = []
+    for i in range(N + 1):
+        if prime[i]:
+            primes.append(i)
 
-        return primes
+    return primes
 
-    def generate_permutation_hash_functions( length, count ):
-        primes = get_sieve(100000))
-        next = 0
+def generate_permutation_hash_functions( length, count ):
+    primes = get_sieve(100000))
+    next = 0
 
-        hash_functions = []
-        for x in range(count):
-            while gcd(length, primes[next]) != 1:
-                next += 1
-            hash_functions.append( { 'a' : primes[next] , 'b' : randint(0, length) }
+    hash_functions = []
+    for x in range(count):
+        while gcd(length, primes[next]) != 1:
             next += 1
+        hash_functions.append( { 'a' : primes[next] , 'b' : randint(0, length) }
+        next += 1
 
-        return hash_functions
+    return hash_functions
 
-    def xor_range_hasher(length):
-        curr = 1
-        while curr < length:
-            curr <<= 1
+def xor_range_hasher(length):
+    curr = 1
+    while curr < length:
+        curr <<= 1
 
-        #partition 0 .. curr into segments
-        func = lambda x : ( x * NUM_HASH_FUNCTIONS ) / curr
-        
-        return func
+    #partition 0 .. curr into segments
+    func = lambda x : ( x * NUM_HASH_FUNCTIONS ) / curr
+    
+    return func
