6cf40d7e0860301238fff152b8aaca8a9eb24c95
AnkithD <cs15btech11016@iith.ac.in>
2018-04-05 14:20:41 +0530

    Fixed some typos in DeclareStories

--- DeclareStories.py
+++ DeclareStories.py
@@ -2,17 +2,17 @@ class DeclareStories:
 	def __init__(self, truePairs, storyCount):
 		self.truePairs = truePairs
 		self.unionSet = [-1 for _ in range(storyCount)]
-	def getParent(index):
-		if self.unionSet[self.index] == -1:
-			return self.index
+	def getParent(self, index):
+		if self.unionSet[index] == -1:
+			return index
 		else:
-			return self.unionSet[self.unionSet[self.index]]
+			return self.unionSet[self.unionSet[index]]
 	def findConnectedComponents(self):
 		for x,y in self.truePairs:
 			x,y = min(x,y), max(x,y)
 			self.unionSet[self.getParent(y)] = self.unionSet[self.getParent(x)]
 		print('Finished generating UnionSet')
-		for i, _ in enumerate(unionSet):
+		for i, _ in enumerate(self.unionSet):
 			self.unionSet[i] = self.getParent(i)
 		print('Finished Optimizaing UnionSet')
 		components = {}
