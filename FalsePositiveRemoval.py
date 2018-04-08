67a98aa53709bfef260b501d340ee000f0066f40
AnkithD <cs15btech11016@iith.ac.in>
2018-04-08 22:34:29 +0530

    Added calculation of average jaccard similarity

--- FalsePositiveRemoval.py
+++ FalsePositiveRemoval.py
@@ -11,13 +11,19 @@ class FalsePositiveRemoval:
 	def RemoveFalsePositives(self):
 		intersect = 0
 		union = 0
+		averageJacSim = 0.0
+		truePositiveCount = 0
 		while self.candidates:
 			a,b = self.candidates.pop()
 			first, second = map(lambda x : set(self.stories[x]['story']), (a,b))
-			if ( len(first & second) / len(first | second) ) > THRESHOLD_JACC_SIMILARITY:
+			jacSim = ( len(first & second) / len(first | second) )
+			if jacSim > THRESHOLD_JACC_SIMILARITY:
+				truePositiveCount += 1
+				averageJacSim += jacSim
 				self.final.append((a,b))
 			else:
 				pass
 				#print(self.titles[a]['title'], 'and', self.titles[b]['title'],'are false positives!')
+		print("Average Jaccard Similarity for true positives is:", (averageJacSim/truePositiveCount))
 		return self.final
 
