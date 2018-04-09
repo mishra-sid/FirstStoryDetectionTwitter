77ebd92a729bcb632b30a481528e56834f41dada
AnkithD <cs15btech11016@iith.ac.in>
2018-04-10 00:56:54 +0530

    Added PCA dimension reduction to 2D

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from MinHash import MinHash
 from LSH import LSH
 from FalsePositiveRemoval import FalsePositiveRemoval
 from DeclareStories import DeclareStories
-
+import numpy
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plot
 
 stories, titles = StoryGenerator("./Dataset").getAllStories()
 tfidf = TFIDF_optim(stories)
@@ -13,30 +15,35 @@ important_words = tfidf.get_important_words()
 print('tfidf completed')
 minHasher = MinHash(tfidf.stories, important_words)
 signature_matrix = minHasher.get_signature_matrix()
-
 print('signature matrix generated')
+sigmat = numpy.array(signature_matrix).T
+pca = PCA(n_components = 2, copy = False)
+reducedVector = pca.fit_transform(sigmat)
+
+plot.scatter(reducedVector[:,0], reducedVector[:,1])
+plot.show()
 
 # print('imp words\n', important_words)
 # print('sig mat\n', signature_matrix)
-lsh = LSH( signature_matrix, stories, important_words)
-candidates = lsh.hash_get_candidates()
-candidatesNum = len(candidates)
-print('candidate pairs generated')
-print('number of candidates:', candidatesNum)
+# lsh = LSH( signature_matrix, stories, important_words)
+# candidates = lsh.hash_get_candidates()
+# candidatesNum = len(candidates)
+# print('candidate pairs generated')
+# print('number of candidates:', candidatesNum)
 
-FPRemover = FalsePositiveRemoval(candidates, tfidf.stories, titles)
-true_pairs = FPRemover.RemoveFalsePositives()
-print('false positives identified')
-print ((1-(len(true_pairs)/candidatesNum)) * 100, 'percent of candidate pairs were false positives')
+# FPRemover = FalsePositiveRemoval(candidates, tfidf.stories, titles)
+# true_pairs = FPRemover.RemoveFalsePositives()
+# print('false positives identified')
+# print ((1-(len(true_pairs)/candidatesNum)) * 100, 'percent of candidate pairs were false positives')
 
-StorySplitter = DeclareStories(true_pairs, len(stories))
-connectedComponents = StorySplitter.findConnectedComponents()
-print('Connected Components have been seperated')
-print('Found', len(connectedComponents), ' connected components of graph')
+# StorySplitter = DeclareStories(true_pairs, len(stories))
+# connectedComponents = StorySplitter.findConnectedComponents()
+# print('Connected Components have been seperated')
+# print('Found', len(connectedComponents), ' connected components of graph')
 
-for value in sorted(connectedComponents.values(), key = lambda l: len(l), reverse = True):
-	#print("Connected Stories:-")
-	for i, story in enumerate(value):
-		if (i >= 5):
-			break
-		#print('\t', titles[story]['title'])
+# for value in sorted(connectedComponents.values(), key = lambda l: len(l), reverse = True):
+# 	#print("Connected Stories:-")
+# 	for i, story in enumerate(value):
+# 		if (i >= 5):
+# 			break
+# 		#print('\t', titles[story]['title'])
