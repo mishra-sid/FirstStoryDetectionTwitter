f1e747845de8593d4c6afae0d81f2101fc47d2b3
sid <es15btech11018@iith.ac.in>
2018-04-18 01:13:49 +0530

    Added clipped threshold community detection

--- community_detection.py
+++ community_detection.py
@@ -1,17 +1,38 @@
 import matplotlib.pyplot as plt
 
 import networkx as nx
-
+from math import sqrt
 import community
+
+from constants import * 
+
 class community_detection:
-    def __init__(self, true_pairs, weights):
-        self.true_pairs = true_pairs
+    def __init__(self, matrix):
         self.G = nx.Graph()
-        for i in range(len(true_pairs)):
-            self.G.add_edge(true_pairs[i][0], true_pairs[i][1], weight=weights[i])
+        self.matrix = matrix
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                matrix[i][j] += 1
+        for i in range(len(self.matrix[0])):
+            for j in range(i + 1, len(self.matrix[0])):
+                edge_weight = self.cosine_similarity(i, j)
+                if edge_weight > THRESHOLD_CLIPPED_SIMILARITY: 
+                    self.G.add_edge(i, j, weight=self.cosine_similarity(i, j))
+    
+    def cosine_similarity(self, i, j):
+        val = 0.0
+        vali = 0.0
+        valj = 0.0
+        for x in range(len(self.matrix)):
+            vali += self.matrix[x][i] * self.matrix[x][i]
+            valj += self.matrix[x][j] * self.matrix[x][j]
+            val += self.matrix[x][i] * self.matrix[x][j] 
+        return val / sqrt(vali * valj)
+
     def detect_community(self):
         partition = community.best_partition(self.G)
         print("Got partition")
+        print(partition.keys())
         size = float(len(set(partition.values())))
         print("size=", size)
         count = 0
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from community_detection import community_detection
 import os
 import pickle
 
+RUN_MODE = 'COMMUNITY'
 
 PICKLE_FILE = './signature_matrix_cache.dat'
 
@@ -33,29 +34,33 @@ else:
     
 # print('imp words\n', important_words)
 # print('sig mat\n', signature_matrix)
-lsh = LSH( signature_matrix, stories, important_words)
-candidates = lsh.hash_get_candidates()
-candidatesNum = len(candidates)
-print('candidate pairs generated')
-print('number of candidates:', candidatesNum)
 
-FPRemover = FalsePositiveRemoval(candidates, tfidf.stories, titles)
-true_pairs, true_weights = FPRemover.RemoveFalsePositives()
-print('false positives identified')
-print ((1-(len(true_pairs)/candidatesNum)) * 100, 'percent of candidate pairs were false positives')
+if RUN_MODE == 'LSH':
+    lsh = LSH( signature_matrix, stories, important_words)
+    candidates = lsh.hash_get_candidates()
+    candidatesNum = len(candidates)
+    print('candidate pairs generated')
+    print('number of candidates:', candidatesNum)
+
+    FPRemover = FalsePositiveRemoval(candidates, tfidf.stories, titles)
+    true_pairs, true_weights = FPRemover.RemoveFalsePositives()
+    print('false positives identified')
+    print ((1-(len(true_pairs)/candidatesNum)) * 100, 'percent of candidate pairs were false positives')
 
 
-StorySplitter = DeclareStories(true_pairs, len(stories))
-connectedComponents = StorySplitter.findConnectedComponents()
-print('Connected Components have been seperated')
-print('Found', len(connectedComponents), ' connected components of graph')
+    StorySplitter = DeclareStories(true_pairs, len(stories))
+    connectedComponents = StorySplitter.findConnectedComponents()
+    print('Connected Components have been seperated')
+    print('Found', len(connectedComponents), ' connected components of graph')
 
-comm = community_detection(true_pairs, true_weights)
-comm.detect_community()
-for value in sorted(connectedComponents.values(), key = lambda l: len(l), reverse = True):
-	#print("Connected Stories:-")
-	for i, story in enumerate(value):
-		if (i >= 5):
-			break
-		#print('\t', titles[story]['title'])
+    for value in sorted(connectedComponents.values(), key = lambda l: len(l), reverse = True):
+        #print("Connected Stories:-")
+        for i, story in enumerate(value):
+                if (i >= 5):
+                        break
+                #print('\t', titles[story]['title'])
+else:
+    comm = community_detection(signature_matrix)
+    comm.detect_community()
+
 
